8f8c5c6adf68fb18dad3e009d4b39d1962bec6a6
Murilo Alves Zapiello <54750759805@info.local>
2024-04-16 14:25:04 -0300

    subindo para a entrega4

---
+++ API-VitalHub/WebAPI/WebAPI/Controllers/ExameController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Domains;
+using WebAPI.Interfaces;
+using WebAPI.Utils.OCR;
+using WebAPI.ViewModels;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExameController : ControllerBase
+    {
+
+        private readonly IExameRepository _exameRepository;
+        private readonly OCRService _ocrService;
+        public ExameController(IExameRepository exameRepository, OCRService ocrService)
+        {
+            _exameRepository = exameRepository;
+            _ocrService = ocrService;
+
+        }
+
+        [HttpPost("Cadastrar")]
+
+        public async Task<IActionResult> Post([FromForm] ExameViewModel exameViewModel)
+        {
+            try
+            {
+                if (exameViewModel.Imagem == null || exameViewModel == null)
+                {
+                    return BadRequest("Nenhuma imagem fornecida");
+                }
+
+                using (var stream = exameViewModel.Imagem.OpenReadStream())
+                {
+                    var result = await _ocrService.RecognizeTextAsync(stream);
+
+                    exameViewModel.Descricao = result;
+
+                    Exame exame = new Exame();
+
+                    exame.Descricao = exameViewModel.Descricao;
+                    exame.ConsultaId = exameViewModel.ConsultaId;
+
+
+
+                    _exameRepository.Cadastrar(exame);
+
+                    return Ok(exame);
+                }
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("BuscarIdConsulta")]
+        public IActionResult GetByIdConsult(Guid idConsulta) 
+        {
+            try
+            {
+                List<Exame> lista = _exameRepository.BuscarPorIdConsulta(idConsulta);
+
+                return Ok(lista);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message );
+            }
+        }
+
+    }
+}
---
+++ API-VitalHub/WebAPI/WebAPI/Utils/OCR/OCRService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace WebAPI.Utils.OCR
+{
+    public class OCRService
+    {
+        private readonly string _subscriptKey = "0662f572663c4b2a92e0f2d25e754af3";
+
+        private readonly string _endpoint = "https://computervisiong11t.cognitiveservices.azure.com/";
+
+
+        public async Task<string> RecognizeTextAsync(Stream imageStream) 
+        {
+            try
+            {
+                var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(_subscriptKey))
+                {
+                    Endpoint = _endpoint
+                };
+
+                var ocrResult = await client.RecognizePrintedTextInStreamAsync(true, imageStream);
+
+                return ProcessRecognitionResult(ocrResult);
+            }
+            catch (Exception e)
+            {
+
+                return "Erro ao reconhecer o texto" +  e.Message;
+            }
+        }
+
+
+        private static string ProcessRecognitionResult(OcrResult result)
+        {
+            try
+            {
+                string recognizedText = "";
+
+                foreach (var region in result.Regions)
+                {
+                    foreach (var line in region.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            recognizedText += word.Text + " ";
+                        }
+
+                        recognizedText += "/n";
+                    }
+                }
+                return recognizedText;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
